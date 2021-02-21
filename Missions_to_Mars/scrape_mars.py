104b909a49e055929a0a53882e58165cedd796c1
Anna Bowers <eenabow@gmail.com>
2021-02-20 20:41:18 -0500

    Starting python code to convert ipynb

---
+++ Missions_to_Mars/scrape_mars.py
@@ -0,0 +1,20 @@
+# Convert Jupyter Notebook to Python 
+#-------------------------------------------------------------
+# Dependencies and Setup
+from bs4 import BeautifulSoup
+from splinter import Browser
+import pandas as pd
+import datetime as dt
+
+# Set Executable Path & Initialize Chrome Browser
+executable_path = {"executable_path": "./chromedriver.exe"}
+browser = Browser("chrome", **executable_path)
+
+#  The default port used by MongoDB is 27017
+conn = 'mongodb://localhost:27017'
+client = pymongo.MongoClient(conn)
+
+# NASA Mars Web Scraper
+#---------------------------------------------------------------
+
+def 

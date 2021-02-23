6cce3bdea2450d5f1ae56faa17aed98305cc5292
Anna Bowers <eenabow@gmail.com>
2021-02-22 22:09:34 -0500

    Starting to fix JPL image

--- Missions_to_Mars/scrape_mars.py
+++ Missions_to_Mars/scrape_mars.py
@@ -30,4 +30,16 @@ def scrape():
     # Create BeautifulSoup object; parse with 'html.parser'
     soup = BeautifulSoup(browser.html, 'html.parser')
 
+    #Find latest news title 
+    news_title = soup.find_all('div', class_='content_title')
+    news_title[1].get_text()
+    title = news_title[1].get_text()
+    mars_data["title"] = title
+
+    #Find latest news description
+    news_p = soup.find_all('div', class_='article_teaser_body')
+    news_p[1].get_text()
+    mars_data["news_p"] = news_p
+
+#COPY REMAINING IPYNB
 return mars_data
